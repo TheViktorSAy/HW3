0758464cde6a06920ed22cda2af76eef9cc02ff2
vkostenko <git@github.com:TheViktorSAy/Hellow-World.git>
2024-09-12 08:59:24 +0300

    Задание 1/2

--- src/Main.java
+++ src/Main.java
@@ -5,20 +5,44 @@ public class Main {
 
         byte a = 100;
         System.out.println(a);
+
         short b = 10000;
         System.out.println(b);
+
         int c = 1000000;
         System.out.println(c);
-        long d = 1000000000;
+
+        long d = 1000000000L;
         System.out.println(d);
+
         float e = 10f;
         System.out.println(e);
-        double f = 11L;
+
+        double f = 11;
         System.out.println(f);
 
         // Задание 2
 
+        float a1 = 27.12f;
+        System.out.println(a1);
 
+        long b1 = 987678965549L;
+        System.out.println(b1);
+
+        double c1 = 2.786;
+        System.out.println(c1);
+
+        short d1 = 569;
+        System.out.println(d1);
+
+        short e1 = -159;
+        System.out.println(e1);
+
+        int f1 = 27897;
+        System.out.println(f1);
+
+        byte g1 = 67;
+        System.out.println(g1);
 
 
 
