1e54a916f14e16a7433e699f03d43a080775ee37
vkostenko <git@github.com:TheViktorSAy/Hellow-World.git>
2024-09-12 08:45:34 +0300

    Задание 1/

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Задание 1
+
+        byte a = 100;
+        System.out.println(a);
+        short b = 10000;
+        System.out.println(b);
+        int c = 1000000;
+        System.out.println(c);
+        long d = 1000000000;
+        System.out.println(d);
+        float e = 10f;
+        System.out.println(e);
+        double f = 11L;
+        System.out.println(f);
+
+        // Задание 2
+
+
+
+
+
+
+
+
     }
 }
